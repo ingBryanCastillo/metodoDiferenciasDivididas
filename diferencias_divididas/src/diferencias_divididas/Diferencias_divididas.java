b9f1193721f390e0c32a33069df46f1abea2e5ed
AlexAvVilla <alex.avila3232@gmail.com>
2023-05-07 17:55:28 -0600

    Se añade el programa

--- diferencias_divididas/src/diferencias_divididas/Diferencias_divididas.java
+++ diferencias_divididas/src/diferencias_divididas/Diferencias_divididas.java
@@ -15,7 +15,6 @@ public class Diferencias_divididas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
