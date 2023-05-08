d2122ddf6f65dffb782e18e4894f0bcc842dee82
trstr <trstr@DESKTOP-395GO8Q>
2023-05-08 01:52:29 -0600

    Todo el metodo terminado

--- diferencias_divididas/src/diferencias_divididas/Diferencias_divididas.java
+++ diferencias_divididas/src/diferencias_divididas/Diferencias_divididas.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package diferencias_divididas;
 
-/**
- *
- * @author Alex
- */
 public class Diferencias_divididas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
     }
     
